ee095e719b41f7367b45f1d492174e9b8779a9af
zaldyprtm <zaldyprtm@gmail.com>
2024-05-13 17:32:49 +0700

    'add'

--- src/components/Data.jsx
+++ src/components/Data.jsx
@@ -1,19 +1,19 @@
 const Data = [
     {
         id: 1,
-        image: "../src/assets/img/featured1.png",
+        image: "/featured1.png",
         name: "Nike Jordan",
         price: "Rp2.408.250,00"
     },
     {
         id: 2,
-        image: "../src/assets/img/featured2.png",
+        image: "/featured2.png",
         name: "Nike Free RN",
         price: "Rp2.408.250,00"
     },
     {
         id: 3,
-        image: "../src/assets/img/featured3.png",
+        image: "/featured3.png",
         name: "Nike Free RN",
         price: "Rp2.408.250,00"
     }
--- src/components/DataWomen.jsx
+++ src/components/DataWomen.jsx
@@ -3,25 +3,25 @@
 const DataWomen = [
     {
         id: 1,
-        image: "../src/assets/img/women1.png",
+        image: "/women1.png",
         name: "Nike Freen TR",
         price: "Rp2.408.250,00",
     },
     {
         id: 2,
-        image: "../src/assets/img/women2.png",
+        image: "/women2.png",
         name: "Nike Freen Orange",
         price: "Rp2.408.250,00",
     },
     {
         id: 3,
-        image: "../src/assets/img/women3.png",
+        image: "/women3.png",
         name: "Nike GS Pink",
         price: "Rp2.408.250,00",
     },
     {
         id: 4,
-        image: "../src/assets/img/women4.png",
+        image: "/women4.png",
         name: "Nike GS Purple",
         price: "Rp2.408.250,00",
     }
